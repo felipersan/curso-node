686aa8346b39d22be2db85ebcfbf5eaf0649bf09
Felipe Sanfilippo <feliperodrigues.san@gmail.com>
2024-09-11 18:29:56 -0300

    feat: NPM course module

---
+++ NPM/Instalando pacotes/index.mjs
@@ -0,0 +1,5 @@
+import chalk from 'chalk'
+
+console.log("Console normal");
+console.log(chalk.green("Console verde"))
+console.log(chalk.red("Console vermelho"))
---
+++ NPM/Segundo projeto/index.js
@@ -0,0 +1,1 @@
+console.log("comando npm init -y")
